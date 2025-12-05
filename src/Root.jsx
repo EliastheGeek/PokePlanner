62c3cd01e9b3cc5e5a0b9e934e141cc767f8c752
Oscar Dalne <oscar.dalne@gmail.com>
2025-12-05 15:50:54 +0100

    test

--- src/Root.jsx
+++ src/Root.jsx
@@ -4,39 +4,20 @@ import { Logout } from "/src/presenters/logoutPresenter.jsx";
 import { SuspenseView } from "/src/views/suspenseView.jsx";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import {useSelector} from "react-redux"
+import MenuBarView from "/src/views/menuBarveiw.jsx";
 
-function makeRouter(){
-    return createHashRouter([
-    {
-        path: "/",
-        element: <Summary />,
-        children: [
-            {
-                path: "main",
-                element: <Summary />
-            }
-        ]
-    },
-])
-}
 
-export function Root(){
-    const user = useSelector((state) => state.poke.user);
-    const ready = useSelector((state) => state.poke.ready);
-    
-    if(user===undefined) return <SuspenseView  promise="notEmpty" />
-    if(user===null) return <Login/>
-    else 
-    {   
-        if (ready) return (
-            <div>
-                <div>
-                    <RouterProvider router={makeRouter()}/> 
-                    <Logout/>
-                </div>
-            </div>
-        );
-        return <div><Logout/> <SuspenseView promise="notEmpty"/></div>
-    }
+
+
+
+
+export function Root() {
+  return (
+    <div className="flex h-full w-full flex-col">
+      <MenuBarView />
+      <div className="h-[900px]">
+      </div>
+    </div>
+  );
 }
 
