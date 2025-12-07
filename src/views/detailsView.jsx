006614462e2982c64b52594844d5b331d3d1cc92
Sparfbag <wilmer.thorn@gmail.com>
2025-12-07 23:50:32 +0100

    abilities

--- src/views/detailsView.jsx
+++ src/views/detailsView.jsx
@@ -3,12 +3,21 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 export function DetailsView(props) {//skicka in en pokemon singular
-
     function backToTeamACB(){
         window.location.hash = "#/team";
     }
     return (
     <div>
+                                <Box
+                                    sx={{
+                                        flex: 1,
+                                        borderLeft: "1px solid #e0e0e0",
+                                        pl: 2,
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        gap: 1,
+                                    }}
+                                >
         <button className="pokeBotBox" onClick={backToTeamACB}>Back to team builder</button>
         <header>
             
@@ -35,6 +44,8 @@ export function DetailsView(props) {//skicka in en pokemon singular
             </div>
         </div>
         {MoveList()}
+        {AbilityList()}
+        </Box>
     </div>
     );
     function printBaseStatsCB(stats) {
@@ -45,85 +56,15 @@ export function DetailsView(props) {//skicka in en pokemon singular
     }
 
 
-
+//TODO, lista ut hur man får ut värdet man väljer från movelist 
 
 function MoveList() {
-
-    const MoveList = [
-    {   move: {
-        name: "volt-tackle",
-        url: "https://pokeapi.co/api/v2/move/344/"
-      },
-    version_group_details: [//array med olika spel, kan vara relevant att antingen välja ett spel i förhand eller låta användaren välja 
-        {
-          level_learned_at: 0,
-          move_learn_method: { name: "tutor"},
-          order: null,
-          version_group: {
-            name: "sun-moon",
-            url: "https://pokeapi.co/api/v2/version-group/17/"
-            }
-        },
-        ],
-    },
-    {
-    move: {
-        name: "shock-wave",
-        url: "https://pokeapi.co/api/v2/move/351/"
-      },
-    version_group_details: [
-        {
-          level_learned_at: 0,
-          move_learn_method: { name: "tutor"},
-          order: null,
-          version_group: {
-            name: "platinum",
-            url: "https://pokeapi.co/api/v2/version-group/9/"
-            }
-        },
-        ],
-    },
-    {
-    move: {
-        name: "quick-attack",
-        url: "https://pokeapi.co/api/v2/move/98/"
-      },
-    version_group_details: [ 
-        {
-          level_learned_at: 13,
-          move_learn_method: { name: "level-up"},
-          order: null,
-          version_group: {
-            name: "black-white",
-            url: "https://pokeapi.co/api/v2/version-group/11/"
-            }
-        },
-        ],
-    },
-    {
-    move: {
-        name: "thunder",
-        url: "https://pokeapi.co/api/v2/move/87/"
-      },
-    version_group_details: [
-        {
-          level_learned_at: 58,
-          move_learn_method: { name: "level-up"},
-          order: null,
-          version_group: {
-            name: "sun-moon",
-            url: "https://pokeapi.co/api/v2/version-group/17/"
-            }
-        },
-        ],
-    }
-    ]
 
  return (
     <Autocomplete
       id="country-select-demo"
       sx={{ width: 200 }}
-      options={MoveList}
+      options={props.pokemon.moves}
       autoHighlight
       getOptionLabel={(option) => option.move.name}
       renderOption={(props, option) => {
@@ -143,6 +84,43 @@ function MoveList() {
         <TextField
           {...params}
           label="Move"
+          slotProps={{
+            htmlInput: {
+              ...params.inputProps,
+              autoComplete: 'new-password', // disable autocomplete and autofill
+            },
+          }}
+        />
+      )}
+    />
+  );
+}
+function AbilityList() {
+
+ return (
+    <Autocomplete
+      id="country-select-demo"
+      sx={{ width: 200 }}
+      options={props.pokemon.abilities}
+      autoHighlight
+      getOptionLabel={(option) => option.ability.name}
+      renderOption={(props, option) => {
+        const { key, ...optionProps } = props;
+        return (
+          <Box
+            key={key}
+            component="li"
+            sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
+            {...optionProps}
+          >
+            {option.ability.name}
+          </Box>
+        );
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Ability"
           slotProps={{
             htmlInput: {
               ...params.inputProps,
