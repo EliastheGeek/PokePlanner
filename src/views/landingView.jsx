9dc4187a94618a934facb1307672ca94789c4b84
Oscar Dalne <oscar.dalne@gmail.com>
2025-12-20 12:51:31 +0100

    css

--- src/views/landingView.jsx
+++ src/views/landingView.jsx
@@ -10,7 +10,7 @@ export function LandingView(props){
                     Create and customize your perfect Pokémon team in one place. Choose items, moves, abilities, 
                     and more with an intuitive team builder designed for competitive and casual players alike. Once your 
                     team is complete, use the integrated AI analysis to evaluate team balance, type coverage, strengths, 
-                    and weaknesses—helping you refine your strategy before battle.
+                    and weaknesses, helping you refine your strategy before battle.
                 </Typography>
 
             </Box>
