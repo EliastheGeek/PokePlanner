e03848b21d29674a6526f978b8b2bce420e46a56
Oscar Dalne <odalne@kth.se>
2025-12-20 13:47:22 +0100

    Team persistance

--- src/reduxStore.js
+++ src/reduxStore.js
@@ -324,6 +324,9 @@ const pokeSlice = createSlice({
         //Just for persistence
         setUser(state, action) { state.user = action.payload; },
         setReady(state, action) { state.ready = action.payload; },
+        setTeam(state, action) { state.team = action.payload; },
+        clearTeam(state) { state.team = [pokemonConst]; },
+
         
         fillFirestore(state, action) {
             //(devComment) Lägg till flera variabler ifall fler sakker ska pushas till FireStore
@@ -408,6 +411,8 @@ export const {
     setCurrentEmail,
     setCurrentPassword,
     setAuthError,
+    setTeam,
+    clearTeam,
 
 
     // Damage calculator
